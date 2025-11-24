721c18831b213df28a4a4cae07def65df3fdb235
tyn7624 <de180524voxuanthanh@gmail.com>
2025-11-24 20:56:42 +0700

    change base

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,38 @@
+import { useSelector } from "react-redux";
+import { Navigate, useRoutes } from "react-router-dom";
+import Layout from "../layout";
+import NotFound from "../pages/NotFound";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+
+const Routers = () => {
+    // const userData = useSelector(state => state.accountReducer);
+    const userData = true;
+    const routes = useRoutes([
+        {
+            path: '/',
+            element: <Layout />,
+            children: [
+                {
+                    index: true,
+                    element: userData? <Navigate to="/home" replace /> : <Navigate to={"/login"}/>
+                },
+                {
+                    path: '/login',
+                    element: <Login/>
+                },
+                {
+                    path: '/home',
+                    element: <Home/>
+                },
+                {
+                    path: '*',
+                    element: <NotFound/>
+                }
+            ]
+        },
+    ])
+    return routes;
+}
+
+export default Routers;
